ef21e7b9862964f669c85613c1b70e71d40a6481
Edwin Biemond <biemond@gmail.com>
2014-04-11 12:01:44 +0200

    5.10 centos

--- README.md
+++ README.md
@@ -7,11 +7,12 @@
 * Solaris 10 x86 64-bit
 
 
-Download packer, tested it with Packer 0.5.1
+Download packer, tested it with Packer 0.5.2
 
-export PATH=~/0.5.1_darwin_amd64:$PATH
+export PATH=~/0.5.2_darwin_amd64:$PATH
 
 * packer build centos-5.8-x86_64.json
+* packer build centos-5.10-x86_64.json
 * packer build centos-6.4-x86_64.json
 * packer build centos-6.5-x86_64.json
 * packer build ubuntu-12.04-amd64.json
--- scripts/puppet-bootstrap/centos_5_x.sh
+++ scripts/puppet-bootstrap/centos_5_x.sh
@@ -4,7 +4,7 @@
 
 set -e
 
-REPO_URL="http://yum.puppetlabs.com/el/5/products/i386/puppetlabs-release-5-7.noarch.rpm"
+REPO_URL="http://yum.puppetlabs.com/el/5/products/x86_64/puppetlabs-release-5-10.noarch.rpm"
 
 if [ "$EUID" -ne "0" ]; then
   echo "This script must be run as root." >&2
@@ -16,6 +16,8 @@ echo "Configuring PuppetLabs repo..."
 repo_path=$(mktemp)
 wget --output-document=${repo_path} ${REPO_URL} 2>/dev/null
 rpm -i ${repo_path} >/dev/null
+
+#yum -y install http://yum.puppetlabs.com/el/5/products/x86_64/puppetlabs-release-5-10.noarch.rpm
 
 # Install Puppet...
 echo "Installing puppet"
