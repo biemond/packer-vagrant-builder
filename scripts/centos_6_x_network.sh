6e2e4feeccf414b0a00c316ff8ee8a0c558a64f2
Edwin Biemond <biemond@gmail.com>
2014-12-24 01:03:29 +0100

    added network cleanup for vmware

---
+++ scripts/centos_6_x_network.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sed -i '/HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i "/^UUID/d" /etc/sysconfig/network-scripts/ifcfg-eth0
+
+rm /etc/sysconfig/selinux
+ln -s /etc/selinux/config /etc/sysconfig/selinux
+sed -i "s/^\(SELINUX=\).*/\1disabled/g" /etc/selinux/config
+
+rm -f /etc/ssh/ssh_host_*
+rm -f /etc/udev/rules.d/70-persistent-net.rules
+rm -f /var/lib/dhclient/dhclient-eth0.leases
---
+++ scripts/centos_7_x_network.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm -f /var/lib/dhclient/dhclient-*.leases
+rm -f /etc/ssh/ssh_host_*
